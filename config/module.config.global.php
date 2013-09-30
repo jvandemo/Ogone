df024b833ea7145de96ce8ddbb4c4dcf6eb925dc
Jurgen Van de Moere <jurgen@webberid.com>
2013-09-30 06:42:41 +0200

    Added global config file

---
+++ config/module.config.global.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'jvandemo-ogone' => array(
+        'PSPID' => 'your_ogone_pspid',
+        'sha1InPassPhrase' => 'your_sha1_in_password',
+        'sha1OutPassPhrase' => 'your_sha1_out_password'
+    )
+);
